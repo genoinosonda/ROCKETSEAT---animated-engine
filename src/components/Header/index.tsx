0e6695cecc665126511042a3c69a0f2d0015b51a
Sonda <genoino.jr@Gmail.com>
2022-03-08 10:45:23 -0300

    Criando componente do cabeçalho

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,3 +1,11 @@
+import styles from './header.module.scss';
+
 export default function Header() {
-  // TODO
+  return (
+    <>
+      <div className={styles.logo}>
+        <img src="/logo.png" alt="logo" />
+      </div>
+    </>
+  );
 }
