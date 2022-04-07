816865e001f7b76e1af2fa66894097eefa5aba23
Sonda <genoino.jr@Gmail.com>
2022-04-06 21:03:50 -0300

    Render dados page
    Realizando o render dos textos obtidos através do CMS

--- src/pages/post/[slug].tsx
+++ src/pages/post/[slug].tsx
@@ -38,25 +38,22 @@ export default function Post(props: PostProps) {
 
       <div className={styles.content}>
         <div className={styles.banner}>
-          <img
-            src="https://images.prismic.io/slicemachine-blank/30d6602b-c832-4379-90ef-100d32c5e4c6_selfie.png?auto=compress,format"
-            alt=""
-          />
+          <img src={props.post.data.banner.url} alt="" />
         </div>
 
         <div className={styles.headerOverview}>
-          <h1>Criando um app CRA do zero</h1>
+          <h1>{props.post.data.title}</h1>
           <ul>
             <li>
               <p>
                 <FiCalendar />
-                15 Mar 2021
+                {props.post.first_publication_date}
               </p>
             </li>
             <li>
               <p>
                 <FiUser />
-                Joseph Oliveira
+                {props.post.data.author}
               </p>
             </li>
             <li>
@@ -68,27 +65,40 @@ export default function Post(props: PostProps) {
         </div>
 
         <div className={styles.newsBody}>
-          <h2>Proin et varius</h2>
-          <p>
-            é simplesmente uma simulação de texto da indústria tipográfica e de
-            impressos, e vem sendo utilizado desde o século XVI, quando um
-            impressor desconhecido pegou uma bandeja de tipos e os embaralhou
-            para fazer um livro de modelos de tipos. Lorem Ipsum sobreviveu não
-            só a cinco séculos, como também ao salto para a editoração
-            eletrônica, permanecendo essencialmente inalterado.
-          </p>
-          <p>
-            Se popularizou na década de 60, quando a Letraset lançou decalques
-            contendo passagens de Lorem Ipsum, e mais recentemente quando passou
-            a ser integrado a softwares de editoração eletrônica como Aldus
-            PageMaker.
-          </p>
-          <p>
-            É um fato conhecido de todos que um leitor se distrairá com o
-            conteúdo de texto legível de uma página quando estiver examinando
-            sua diagramação.
-          </p>
+          {props.post.data.content.map(contentBody => (
+            <>
+              <h2 key={contentBody.heading}>{contentBody.heading[0].text}</h2>
+              {contentBody.body.map(bodyPost =>(
+                <p>{bodyPost.text}</p>
+              ))}
+
+            </>
+          ))}
         </div>
+
+        {/*
+          <div className={styles.newsBody}>
+            <h2>Proin et varius</h2>
+            <p>
+              é simplesmente uma simulação de texto da indústria tipográfica e de
+              impressos, e vem sendo utilizado desde o século XVI, quando um
+              impressor desconhecido pegou uma bandeja de tipos e os embaralhou
+              para fazer um livro de modelos de tipos. Lorem Ipsum sobreviveu não
+              só a cinco séculos, como também ao salto para a editoração
+              eletrônica, permanecendo essencialmente inalterado.
+            </p>
+            <p>
+              Se popularizou na década de 60, quando a Letraset lançou decalques
+              contendo passagens de Lorem Ipsum, e mais recentemente quando passou
+              a ser integrado a softwares de editoração eletrônica como Aldus
+              PageMaker.
+            </p>
+            <p>
+              É um fato conhecido de todos que um leitor se distrairá com o
+              conteúdo de texto legível de uma página quando estiver examinando
+              sua diagramação.
+            </p>
+          </div>
 
         <div className={styles.newsBody}>
           <h2>Lorem ipsum</h2>
@@ -112,6 +122,7 @@ export default function Post(props: PostProps) {
             eletrônica, permanecendo essencialmente inalterado.{' '}
           </p>
         </div>
+         */}
       </div>
     </>
   );
@@ -130,62 +141,24 @@ export const getStaticPaths = async () => {
 };
 
 export const getStaticProps = async context => {
-  console.log(context);
-
   const uid = context.params.slug;
 
   const prismic = getPrismicClient();
   const response = await prismic.getByUID('posts', String(uid), {});
 
-  //console.log(response);
-  //console.log(response.data);
-
-  //console.log(response.data.content);
-
-  const valor = response.data.content;
-
-
-  valor.map(post => {
-    console.log(post);
-  });
-
-  /*
   const post: Post = {
     first_publication_date: response.first_publication_date,
-    data:{
-      title: response.data.title,
-      banner:{
-        url: response.data.banner.url
+    data: {
+      title: response.data.title[0].text,
+      banner: {
+        url: response.data.banner.url,
       },
-      author: response.data.author,
-      content: {
-        heading:
-      }
-    }
+      author: response.data.author[0].text,
+      content: response.data.content,
+    },
   };
-*/
-
-  /*
-interface Post {
-  first_publication_date: string | null;
-  data: {
-    title: string;
-    banner: {
-      url: string;
-    };
-    author: string;
-    content: {
-      heading: string;
-      body: {
-        text: string;
-      }[];
-    }[];
-  };
-}
-  */
 
   return {
-    props: {},
+    props: { post },
   };
-  //   // TODO
 };
