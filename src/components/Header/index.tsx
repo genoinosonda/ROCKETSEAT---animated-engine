6bf7d5821bf5ccb22f7d51208e181fa9f56d05f7
Sonda <genoino.jr@Gmail.com>
2022-04-11 21:19:02 -0300

    implementando link no cabeçalho

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,11 +1,14 @@
 import styles from './header.module.scss';
+import Link from 'next/link';
 
 export default function Header() {
   return (
     <>
-      <div className={styles.logo}>
-        <img src="/logo.png" alt="logo" />
-      </div>
+      <Link href="/">
+        <div className={styles.logo}>
+          <img src="/logo.png" alt="logo" />
+        </div>
+      </Link>
     </>
   );
 }
